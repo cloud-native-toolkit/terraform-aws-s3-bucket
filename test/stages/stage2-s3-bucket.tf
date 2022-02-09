8ccd172f4581725e5a374a3000502f4d13894233
Sasikanth G <sgumpana@gmail.com>
2022-02-09 22:07:59 +0530

    File upload to AWS S3 Bucket
    Signed-off-by: Sasikanth G <sgumpana@gmail.com>

--- test/stages/stage2-s3-bucket.tf
+++ test/stages/stage2-s3-bucket.tf
@@ -1,5 +1,4 @@
 module "aws-s3-bucket" {
-  depends_on = [ module.aws_s3_instance]
   source = "./module"
   bucket_id = module.aws_s3_instance.s3_bucket_id
   s3_bucket_region = module.aws_s3_instance.s3_bucket_region
